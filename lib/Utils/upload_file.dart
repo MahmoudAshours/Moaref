dc9f74558e964c4b82a7fd4a8e609aa8afb01ee4
Your Name <mashour365@gmail.com>
2020-12-28 15:59:47 +0200

    Finished Upload File UI

--- lib/Utils/upload_file.dart
+++ lib/Utils/upload_file.dart
@@ -1,9 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:ffmpegtest/Themes/theme.dart';
+import 'package:ffmpegtest/Utils/seach.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class UploadFile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          textDirection: TextDirection.rtl,
+          //TODO Fix search!!
+          children: [_languagePicker(), _categoryPicker(), Search()],
+        ),
+        SizedBox(height: 20),
+        FadeInUp(
+          duration: Duration(milliseconds: 500),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: RaisedButton(
+                onPressed: () {},
+                color: kSecondaryColor,
+                child: Text('إضافة ملف صوتي',
+                    style: TextStyle(color: Colors.white)),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0),
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 7),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'هذه الخاصية تمكنك من إضافة مقطع صوتي لاستخرجه كمقطع دعوي',
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  _languagePicker() {
     return Container(
-      
+      width: 100,
+      height: 50,
+      //Changes when adding API
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black26),
+          borderRadius: BorderRadius.circular(10)),
+      child: Center(
+        child: Text(
+          'عربي',
+          style: TextStyle(
+            color: kFontColor,
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _categoryPicker() {
+    return Container(
+      width: 200,
+      height: 50,
+      //Changes when adding API
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black26),
+          borderRadius: BorderRadius.circular(10)),
+      child: Center(
+        child: Text(
+          'التصنيف / مقاطع دعوية',
+          style: TextStyle(
+            color: kFontColor,
+            fontWeight: FontWeight.w700,
+            fontSize: 19,
+          ),
+        ),
+      ),
     );
   }
 }
