cb965950f8374bda817dacdf5c1f4df1134b2100
Your Name <mashour365@gmail.com>
2020-12-28 17:34:46 +0200

    Finished Gallery UI

--- lib/Themes/theme.dart
+++ lib/Themes/theme.dart
@@ -4,3 +4,4 @@ const kPrimaryColor = Colors.white;
 const kSecondaryColor = Color(0xff45842D);
 const kFontColor = Color(0xffD3B76C);
 const kSecondaryFontColor = Color(0xff557281);
+const kBackgroundIconColor = Color(0xff2C2C2C);
--- lib/Utils/gallery.dart
+++ lib/Utils/gallery.dart
@@ -1,8 +1,93 @@
+import 'package:ffmpegtest/Themes/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Gallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2.9,
+          child: GridView.count(
+            crossAxisCount: 5,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            padding: EdgeInsets.all(8),
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: kBackgroundIconColor,
+                  borderRadius: BorderRadius.circular(7),
+                ),
+                child: Icon(
+                  Icons.folder_open_rounded,
+                  size: 40,
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.blue,
+              ),
+              Container(
+                color: Colors.black,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
