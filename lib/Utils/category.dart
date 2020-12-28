8782f067013c1b081ad726fb4b2b4b87b9f6a4d6
Your Name <mashour365@gmail.com>
2020-12-28 15:26:02 +0200

    Added animate do to categories

--- lib/Utils/category.dart
+++ lib/Utils/category.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:ffmpegtest/Themes/theme.dart';
 import 'package:ffmpegtest/Utils/seach.dart';
 import 'package:flutter/material.dart';
@@ -15,39 +16,45 @@ class Category extends StatelessWidget {
         ),
         SizedBox(height: 10),
         Container(
-          height: MediaQuery.of(context).size.height/4.4,
+          height: MediaQuery.of(context).size.height / 4.4,
           child: ListView.separated(
             separatorBuilder: (c, i) {
-              return Divider(
-                thickness: 2,
-                color: Colors.black12,
+              return FadeInUpBig(
+                delay: Duration(milliseconds: 100 * i),
+                child: Divider(
+                  thickness: 2,
+                  color: Colors.black12,
+                ),
               );
             },
             itemCount: 12,
-            itemBuilder: (c, i) {
+            itemBuilder: (BuildContext context, int i) {
               print(i);
               return SafeArea(
                 child: Padding(
                   padding: const EdgeInsets.all(2.0),
-                  child: ListTile(
-                    leading: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: kSecondaryColor,
-                      ),
-                      child: Center(
-                        child: FaIcon(
-                          FontAwesomeIcons.play,
-                          color: Colors.white,
+                  child: FadeInUp(
+                    delay: Duration(milliseconds: 100 * i),
+                    child: ListTile(
+                      leading: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: kSecondaryColor,
+                        ),
+                        child: Center(
+                          child: FaIcon(
+                            FontAwesomeIcons.play,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    title: Text(
-                      'رحمة النبي',
-                      style: TextStyle(color: kSecondaryFontColor),
-                      textDirection: TextDirection.rtl,
+                      title: Text(
+                        'رحمة النبي',
+                        style: TextStyle(color: kSecondaryFontColor),
+                        textDirection: TextDirection.rtl,
+                      ),
                     ),
                   ),
                 ),
