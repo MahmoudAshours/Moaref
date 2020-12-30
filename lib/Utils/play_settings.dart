9e1e51b8e213ab20bab039b8e13b6d55a017d6cf
Your Name <mashour365@gmail.com>
2020-12-30 18:05:19 +0200

    Bug fixes

--- lib/Utils/play_settings.dart
+++ lib/Utils/play_settings.dart
@@ -4,12 +4,11 @@ class PlaySettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 150,
-      height: 40,
+      width: 190,
+      height: 45,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-
           Padding(
             padding: const EdgeInsets.all(1.0),
             child: Image.asset('assets/Images/render.png'),
