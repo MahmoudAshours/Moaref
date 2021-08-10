fcd6039ccb359e9c21ad0c04c1b99db6ea714ebd
Mahmoud Ashour <mashour365@gmail.com>
2021-08-10 12:56:44 +0200

    Fixed UTF-8 encoding #4

--- lib/Helpers/link_manipulation.dart
+++ lib/Helpers/link_manipulation.dart
@@ -1,27 +1,28 @@
 import 'package:html/dom.dart';
 
-List<String> linkManipulator(Document document) {
-  List<String> manipulation = [];
-  for (int i = 5; i < document.getElementsByTagName('a').length; i++) {
-    String? x = document.getElementsByTagName('a')[i].attributes['href'];
-    x = x!.replaceAll('%20', ' ');
-    x = x.replaceAll('%e2%80%99', "'");
-    x = x.replaceAll('/', '');
-    x = Uri.decodeComponent(x);
-    manipulation.add(x);
+List<String> linkDecoder(Document document) {
+  List<String> _decodedLinks = [];
+  for (int _documentLinkIterator = 5;
+      _documentLinkIterator < document.getElementsByTagName('a').length;
+      _documentLinkIterator++) {
+    String? _link = document
+        .getElementsByTagName('a')[_documentLinkIterator]
+        .attributes['href'];
+    _link = Uri.decodeFull(_link!).replaceAll('/', '');
+    print(_link);
+    _decodedLinks.add(_link);
   }
-  return manipulation;
+  return _decodedLinks;
 }
 
-List<String> languageManipulator(Document document) {
-  List<String> manipulation = [];
+List<String> languageDecoder(Document document) {
+  List<String> _decodedLangs = [];
   for (var i = 5; i < document.getElementsByTagName('a').length - 1; i++) {
-    String? x = document.getElementsByTagName('a')[i].attributes['href'];
-    x = x!.replaceAll('%20', ' ');
-    x = x.replaceAll('%e2%80%99', "'");
-    x = x.replaceAll('/', '');
-    x = Uri.decodeComponent(x);
-    manipulation.add(x);
+    String? _language =
+        document.getElementsByTagName('a')[i].attributes['href'];
+    _language = Uri.decodeFull(_language!).replaceAll('/', '');
+    print(_language);
+    _decodedLangs.add(_language);
   }
-  return manipulation;
+  return _decodedLangs;
 }
