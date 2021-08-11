d415ded17831bf29a5c11d6a11fc995ac9a3b75d
Mahmoud Ashour <mashour365@gmail.com>
2021-08-11 14:17:18 +0200

    Fixed todo comments

--- lib/Screens/generated_plugin_registrant.dart
+++ lib/Screens/generated_plugin_registrant.dart
@@ -10,7 +10,9 @@ import 'package:video_player_web/video_player_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
+// ignore: deprecated_member_use
 void registerPlugins(PluginRegistry registry) {
+  // ignore: deprecated_member_use
   VideoPlayerPlugin.registerWith(registry.registrarFor(VideoPlayerPlugin));
   registry.registerMessageHandler();
 }
