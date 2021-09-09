33f132003029946c9643f4d198c7e7f8800e36e6
Mahmoud Ashour <mashour365@gmail.com>
2021-09-09 18:59:03 +0200

    Code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:konmoaref/Helpers/asset_to_file.dart';
+import 'package:konmoaref/Provider/MacosProvider/ffmpeg_macprovider.dart';
 import 'package:konmoaref/Provider/data_provider.dart';
 import 'package:konmoaref/Provider/ffmpeg_provider.dart';
 import 'package:konmoaref/Provider/gallery_provider.dart';
+import 'package:konmoaref/Provider/orientation_provider.dart';
 import 'package:konmoaref/Provider/player_provider.dart';
 import 'package:konmoaref/Provider/record_provider.dart';
 import 'package:konmoaref/Provider/upload_provider.dart';
+import 'package:konmoaref/Screens/home_screen.dart';
 import 'package:konmoaref/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -27,11 +32,13 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => UploadProvider()),
         ChangeNotifierProvider(create: (_) => GalleryProvider()),
         ChangeNotifierProvider(create: (_) => PlayerProvider()),
-        ChangeNotifierProvider(create: (_) => FfmpegProvider())
+        ChangeNotifierProvider(create: (_) => FfmpegProvider()),
+        ChangeNotifierProvider(create: (_) => OrientationProvider()),
+        ChangeNotifierProvider(create: (_) => FFmpegMacProvider())
       ],
       child: MaterialApp(
         title: 'كن معرفا برسول الله ',
-        home: SplashScreen(splashVideo),
+        home: Platform.isMacOS ? HomeScreen() : SplashScreen(splashVideo),
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'NeoSansArabic'),
       ),
