b56c157d606ef40e4843f5834faf7a8fe00b80ef
Mahmoud Ashour <mashour365@gmail.com>
2021-09-09 18:58:30 +0200

    Code refactor

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,15 +1,9 @@
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:konmoaref/Provider/data_provider.dart';
-import 'package:konmoaref/Provider/gallery_provider.dart';
-import 'package:konmoaref/Provider/record_provider.dart';
-import 'package:konmoaref/Provider/upload_provider.dart';
+import 'dart:io';
+
 import 'package:konmoaref/Screens/AudioUpload/upload_file.dart';
 import 'package:konmoaref/Screens/Categories/category_screen.dart';
-import 'package:konmoaref/Screens/FFmpegOps/ffmpegop.dart';
-import 'package:konmoaref/Screens/Gallery/gallery.dart';
 import 'package:konmoaref/Screens/Recording/recording.dart';
 import 'package:konmoaref/Themes/theme.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,42 +11,11 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>
-    with AutomaticKeepAliveClientMixin<HomeScreen> {
-  @override
-  bool get wantKeepAlive => true;
-
-  late DataProvider _dataProvider;
-  late UploadProvider _uploadProvider;
-  late RecordProvider _recordProvider;
-  late GalleryProvider _galleryProvider;
-  bool _audioReady = false;
-  bool _videoReady = false;
+class _HomeScreenState extends State<HomeScreen> {
   double ready = 0;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() async {
-    _dataProvider = Provider.of<DataProvider>(context, listen: true);
-    _uploadProvider = Provider.of<UploadProvider>(context, listen: true);
-    _recordProvider = Provider.of<RecordProvider>(context, listen: true);
-    _galleryProvider = Provider.of<GalleryProvider>(context, listen: true);
-    super.didChangeDependencies();
-  }
-
-  @override
-  void dispose() {
-    _dataProvider.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    _readyIndicator();
-    super.build(context);
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
@@ -84,132 +47,95 @@ class _HomeScreenState extends State<HomeScreen>
               ),
             ),
             Positioned(
-              top: MediaQuery.of(context).size.height / 5,
+              top: MediaQuery.of(context).size.height / 4,
               left: MediaQuery.of(context).size.width / 4.8,
               child: Padding(
                 padding: const EdgeInsets.only(top: 15.0),
                 child: Container(
                   width: MediaQuery.of(context).size.width / 1.5,
-                  height: MediaQuery.of(context).size.height / 1,
-                  child: GridView.count(
-                    crossAxisSpacing: MediaQuery.of(context).size.width / 7,
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 30,
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      GestureDetector(
-                        onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(builder: (_) => Recording())),
-                        child: _gridViewItems(
-                          activeAssetPath: 'assets/Images/mic.png',
-                          label: 'تسجيل',
+                  height: MediaQuery.of(context).size.height / 1.2,
+                  child: Platform.isMacOS
+                      ? GridView.count(
+                          crossAxisSpacing:
+                              MediaQuery.of(context).size.width / 8,
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 40,
+                          physics: NeverScrollableScrollPhysics(),
+                          children: [
+                              GestureDetector(
+                                onTap: () => Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (_) => CategoryScreen())),
+                                child: _gridViewItems(
+                                  activeAssetPath:
+                                      'assets/Images/category_icon.png',
+                                  label: 'التصنيف',
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () => Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (_) => UploadFile())),
+                                child: _gridViewItems(
+                                  activeAssetPath:
+                                      'assets/Images/upload_file.png',
+                                  label: 'ملف صوتي',
+                                ),
+                              )
+                            ])
+                      : GridView.count(
+                          crossAxisSpacing:
+                              MediaQuery.of(context).size.width / 7,
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 30,
+                          physics: NeverScrollableScrollPhysics(),
+                          children: [
+                            GestureDetector(
+                              onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (_) => Recording())),
+                              child: _gridViewItems(
+                                activeAssetPath: 'assets/Images/mic.png',
+                                label: 'تسجيل',
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (_) => CategoryScreen())),
+                              child: _gridViewItems(
+                                activeAssetPath:
+                                    'assets/Images/category_icon.png',
+                                label: 'التصنيف',
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () => Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                      builder: (_) => UploadFile())),
+                              child: _gridViewItems(
+                                activeAssetPath:
+                                    'assets/Images/upload_file.png',
+                                label: 'ملف صوتي',
+                              ),
+                            ),
+                            // _gridViewItems(
+                            //   activeAssetPath: 'assets/Images/information_button.png',
+                            //   label: 'عن التطبيق',
+                            // ),
+                            // _gridViewItems(
+                            //   activeAssetPath: 'assets/Images/smartphone_icon.png',
+                            //   label: 'عن التطبيق',
+                            // ),
+                          ],
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (_) => CategoryScreen())),
-                        child: _gridViewItems(
-                          activeAssetPath: 'assets/Images/category_icon.png',
-                          label: 'التصنيف',
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(builder: (_) => UploadFile())),
-                        child: _gridViewItems(
-                          activeAssetPath: 'assets/Images/upload_file.png',
-                          label: 'ملف صوتي',
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () => Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (_) => Gallery())),
-                        child: _gridViewItems(
-                          activeAssetPath: 'assets/Images/video_library.png',
-                          label: 'المكتبة',
-                        ),
-                      ),
-                      _gridViewItems(
-                        activeAssetPath: 'assets/Images/information_button.png',
-                        label: 'عن التطبيق',
-                      ),
-                      _gridViewItems(
-                        activeAssetPath: 'assets/Images/smartphone_icon.png',
-                        label: 'عن التطبيق',
-                      ),
-                    ],
-                  ),
                 ),
               ),
             ),
-            SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GestureDetector(
-                  onTap: () => _readyIndicator() == 1.0
-                      ? Navigator.of(context).push(
-                          MaterialPageRoute(builder: (_) => FFmpegOperations()))
-                      : Fluttertoast.showToast(
-                          msg: "يجب عليك الانتهاء من اختيار المقطع و الصوت",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 5,
-                          backgroundColor: Colors.white,
-                          textColor: Colors.black,
-                          fontSize: 15.0),
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Stack(
-                      fit: StackFit.passthrough,
-                      children: [
-                        Container(
-                            child: CircularProgressIndicator(
-                                value: _readyIndicator(),
-                                color: _readyIndicator() == 1.0
-                                    ? Colors.orange
-                                    : Colors.blue),
-                            width: 40,
-                            height: 40),
-                        Icon(
-                          Icons.play_arrow,
-                          size: 40,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            )
           ],
         ),
       ),
     );
-  }
-
-  double? _readyIndicator() {
-    final _cloudAudioReady = _dataProvider.cloudAudioPicked != null &&
-        _dataProvider.cloudAudioPicked!.isNotEmpty;
-    final _recordingAudioReady = _recordProvider.recordPath != null &&
-        _recordProvider.recordPath!.isNotEmpty;
-    final _uploadedAudioReady = _uploadProvider.uploadedAudioPath != null &&
-        _uploadProvider.uploadedAudioPath!.isNotEmpty;
-    final _galleryVideoPath = _galleryProvider.videoPath != null &&
-        _galleryProvider.videoPath!.isNotEmpty;
-
-    if (_cloudAudioReady || _recordingAudioReady || _uploadedAudioReady) {
-      _audioReady = true;
-    } else {
-      _audioReady = false;
-    }
-    if (_galleryVideoPath) {
-      _videoReady = true;
-    } else {
-      _videoReady = false;
-    }
-    if (_audioReady && _videoReady) return 1.0;
-    if (!_audioReady && !_videoReady) return 0.0;
-    return 0.5;
   }
 
   Container _gridViewItems(
